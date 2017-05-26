619a82ee9ea3aa5b57fd9c0e346943b8bcc9a7ee
Paul Vander <paulsvander@gmail.com>
2017-05-25 18:30:15 -0700

    Elements in correct position

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,17 +2,21 @@ require 'rest-client'
 require 'json'
 
 get '/' do
-  erb :'index'
+  options = {width: 1280, height: 720, channel: "ESL_Hearthstone"}
+  erb :'index', locals: {options: options}
+end
+
+post '/' do
+  options = {width: 1280, height: 720, channel: "#{params[:channel]}"}
+  erb :'index', locals: {options: options}
 end
 
 get '/card' do
-  puts "REACHED 2"
   params[:card].gsub!(" ", "%20")
   response = RestClient.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/#{params[:card]}", headers = {"X-Mashape-Key": ENV["HEARTHSTONE_TOKEN"]})
   response = JSON.parse(response)
-  puts "REACHED 3"
+
   if request.xhr?
-    puts "REACHED 4"
     erb :show_card, layout: false, locals: {response: response[0]}
   else
     erb :'show_card', locals: {response: response[0]}
--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
   $(".card-search").submit(function(e) {
-    console.log("REACHED 1")
     e.preventDefault();
+    $(".card-display").show()
     var url = $(this).attr("action")
     var method = $(this).attr("method")
     var data = $(this).serialize()
@@ -17,7 +13,15 @@ $(document).ready(function() {
     })
     .done(function(response) {
       $(".card-display").html(response)
+      $(".card-search")[0].reset()
     })
-    $("#card-img").attr("src","http://www.hearthstonetopdecks.com/wp-content/uploads/2014/06/card-back-default.png")
+
+    $("#card-img").attr({
+      src: "http://www.hearthstonetopdecks.com/wp-content/uploads/2014/06/card-back-default.png",
+      width: 290,
+      height: 412
+    })
+
+    $("#card-details-list").html(" ")
   })
 });
